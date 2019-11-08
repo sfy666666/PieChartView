138b2fb01ac918d1fc5c88de78162cee538b0dbf
Administrator <1352324022@qq.com>
2019-11-08 18:42:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yanxu/piechartview/activity/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.yanxu.piechartview.activity;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.yanxu.piechartview.R;
+import com.example.yanxu.piechartview.view.PieChartView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PieChartView mPieChartView;
+    private float[] ratios = {0.8f, 0.2f};
+    //    private float[] ratios = {0.1f, 0.2f, 0.3f, 0.4f};
+    private String[] colors = {"#6F7CFF", "#FFB864"};
+    //    private String[] colors = {"#FFB864", "#6F7CFF", "#884EDB", "#90E3E1"};
+    static int progress = 80;
+    static int current=0;
+    private static TextView tvProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    /**
+     * 初始化view
+     */
+    private void initView() {
+        mPieChartView = findViewById(R.id.pie_chart_view);
+        tvProgress = findViewById(R.id.tv_progress);
+    }
+
+    /**
+     * 初始化data
+     */
+    private void initData() {
+        if (ratios.length == 1)
+            colors = null;
+        mPieChartView.initSrc(ratios, colors);
+        mPieChartView.startDraw();
+        mHandler.sendEmptyMessage(0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
+    }
+
+    public static Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg!=null){
+                tvProgress.setText(msg.what+"%");
+                if (current <= progress)
+                    mHandler.sendEmptyMessage(current++);
+                else
+                    mHandler.removeCallbacksAndMessages(null);
+            }
+
+        }
+    };
+
+}
